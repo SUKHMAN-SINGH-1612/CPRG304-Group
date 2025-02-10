340d16c1a99008f80939b716a7e86eb685156c24
SantiagoCardenasX <santiagocardenass@hotmail.com>
2025-02-10 14:33:14 -0700

    Documentation added, exercise 1 done

--- Lab2/src/exercise1/Driver.java
+++ Lab2/src/exercise1/Driver.java
@@ -1,42 +1,54 @@
+
 package exercise1;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Driver
-{
+/**
+ * The Driver class contains the main method to demonstrate sorting of Student objects.
+ */
+public class Driver {
 
-	public static void main( String[] args )
-	{
+    /**
+     * The main method creates a list of Student objects, prints the original list,
+     * sorts the list by age, prints the sorted list, sorts the list by name, and prints the sorted list.
+     *
+     * @param args command-line arguments (not used)
+     */
+    public static void main(String[] args) {
 
-		ArrayList<Student> studs = new ArrayList<>();
-		studs.add( new Student( "Smith", 34 ) );
-		studs.add( new Student( "Johnson", 21 ) );
-		studs.add( new Student( "Williams", 67 ) );
-		studs.add( new Student( "Brown", 53 ) );
-		studs.add( new Student( "Jones", 48 ) );
-		studs.add( new Student( "Miller", 36 ) );
-		studs.add( new Student( "Davis", 44 ) );
-		studs.add( new Student( "Wilson", 52 ) );
-		studs.add( new Student( "Anderson", 34 ) );
-		studs.add( new Student( "Moore", 33 ) );
+        // Create an ArrayList of Student objects
+        ArrayList<Student> studs = new ArrayList<>();
+        studs.add(new Student("Smith", 34));
+        studs.add(new Student("Johnson", 21));
+        studs.add(new Student("Williams", 67));
+        studs.add(new Student("Brown", 53));
+        studs.add(new Student("Jones", 48));
+        studs.add(new Student("Miller", 36));
+        studs.add(new Student("Davis", 44));
+        studs.add(new Student("Wilson", 52));
+        studs.add(new Student("Anderson", 34));
+        studs.add(new Student("Moore", 33));
 
-        System.out.println( "Original list:" );
-		for (Student s : studs)
-			System.out.println(s);
-		
-		Collections.sort( studs, Student.ageComparator );
-		
-		System.out.println( "\nSorted by age:" );
-		for (Student s : studs)
-			System.out.println(s);
-		
-		Collections.sort( studs, Student::compareTo );
-        
-        System.out.println( "\nSorted by name:" );
-		for (Student s : studs)
-			System.out.println(s);
-				
-	}
+        // Print the original list of students
+        System.out.println("Original list:");
+        for (Student s : studs)
+            System.out.println(s);
 
+        // Sort the list of students by age using the ageComparator
+        Collections.sort(studs, Student.ageComparator);
+
+        // Print the list of students sorted by age
+        System.out.println("\nSorted by age:");
+        for (Student s : studs)
+            System.out.println(s);
+
+        // Sort the list of students by name using the compareTo method
+        Collections.sort(studs, Student::compareTo);
+
+        // Print the list of students sorted by name
+        System.out.println("\nSorted by name:");
+        for (Student s : studs)
+            System.out.println(s);
+    }
 }
--- Lab2/src/exercise1/Student.java
+++ Lab2/src/exercise1/Student.java
@@ -1,49 +1,77 @@
+
 package exercise1;
+
 import java.util.Comparator;
 
-public class Student implements Comparable<Student>
-{
-	private String name;
-	private int age;
-	
-	public Student( String name, int age )
-	{
-		this.name = name;
-		this.age = age;
-	}
-	
-	public String getName()
-	{
-		return name;
-	}
-	
-	public int getAge()
-	{
-		return age;
-	}
-	
-	@Override
-	public int compareTo(Student other)
-	{
-		return name.compareTo(other.name);
-	}
+/**
+ * The Student class represents a student with a name and age.
+ * It implements the Comparable interface to allow comparison by name.
+ */
+public class Student implements Comparable<Student> {
+    private String name;
+    private int age;
 
-	public static Comparator<Student> ageComparator = new Comparator<Student>()
-	{
-		@Override
-		public int compare(Student s1, Student s2)
-		{
-			if (s1.getAge() == s2.getAge())
-				return s1.getName().compareTo(s2.getName());
-			else
-				return Integer.compare(s1.getAge(), s2.getAge());
-		}
-	};
-	
-	@Override
-	public String toString()
-	{
-		return name + " " + age;
-	}
-	
+    /**
+     * Constructs a new Student with the specified name and age.
+     *
+     * @param name the name of the student
+     * @param age  the age of the student
+     */
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    /**
+     * Returns the name of the student.
+     *
+     * @return the name of the student
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the age of the student.
+     *
+     * @return the age of the student
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * Compares this student to another student by name.
+     *
+     * @param other the student to be compared
+     * @return a negative integer, zero, or a positive integer as this student's name
+     *         is less than, equal to, or greater than the specified student's name
+     */
+    @Override
+    public int compareTo(Student other) {
+        return name.compareTo(other.name);
+    }
+
+    /**
+     * A comparator to compare students by age. If ages are equal, compares by name.
+     */
+    public static Comparator<Student> ageComparator = new Comparator<Student>() {
+        @Override
+        public int compare(Student s1, Student s2) {
+            if (s1.getAge() == s2.getAge())
+                return s1.getName().compareTo(s2.getName());
+            else
+                return Integer.compare(s1.getAge(), s2.getAge());
+        }
+    };
+
+    /**
+     * Returns a string representation of the student.
+     *
+     * @return a string representation of the student in the format "name age"
+     */
+    @Override
+    public String toString() {
+        return name + " " + age;
+    }
 }
