1a267c1a7ba8dc07330e015f4e8a6bf3d7fea32c
Sukhman Singh <sukhmansinghbhogal@gmail.com>
2025-02-28 01:50:04 -0700

    its working but not sure its working correctly or not

--- Assignment1/src/sortingAlgorithms/SortingUtility.java
+++ Assignment1/src/sortingAlgorithms/SortingUtility.java
@@ -1,6 +1,32 @@
+
 package sortingAlgorithms;
-public class SortingUtility
-{
-	// IMPORTANT
-	// Algorithms must use comparable for height and comparators for other orders
+
+import java.util.Comparator;
+import shapeManager.Shape;
+
+public class SortingUtility {
+
+    public static void bubbleSort(Shape[] array, Comparator<Shape> comp) {
+        BubbleSort.sort(array, comp);
+    }
+
+    public static void insertionSort(Shape[] array, Comparator<Shape> comp) {
+        InsertionSort.sort(array, comp);
+    }
+
+    public static void selectionSort(Shape[] array, Comparator<Shape> comp) {
+        SelectionSort.sort(array, comp);
+    }
+
+    public static void mergeSort(Shape[] array, Comparator<Shape> comp) {
+        MergeSort.sort(array, comp);
+    }
+
+    public static void quickSort(Shape[] array, Comparator<Shape> comp) {
+        QuickSort.sort(array, comp);
+    }
+
+    public static void countingSort(Shape[] array, Comparator<Shape> comp) {
+        CountingSort.sort(array);
+    }
 }
